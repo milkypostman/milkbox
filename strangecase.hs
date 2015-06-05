44ac830dd8ad9fde999d09492807b7ea7ce6c685
Donald Curtis <d@milkbox.net>
2015-06-04 20:48:51 -0400

    properly split the input

--- strangecase.hs
+++ strangecase.hs
@@ -2,25 +2,19 @@
 import Data.Char(toUpper)
 import Data.List.Split(splitOn)
 
+addToFirst :: String -> (String, String) -> (String, String)
+addToFirst x (y, ys) = (x ++ y, ys)
+
 -- Split the array of strings into two types. The front matter and the
 -- entry itself.
 getEntry' :: [String] -> (String, String)
 getEntry' [] = ("", "")
 getEntry' (x:xs) | ("---" == x) = ("", unlines $ dropWhile ((==) "") xs)
-                 | otherwise = cons x $ getEntry' xs
-  where cons z (y, ys) = (z ++ y, ys)
+                 | otherwise = addToFirst x $ getEntry' xs
 
 getEntry :: [String] -> (String, String)
 getEntry (x:xs) | ("---" == x) = getEntry' xs
-                    | otherwise = getEntry xs
+                | otherwise = getEntry xs
 
-main =
-  putStrLn $ show $ getEntry [
-    "part before this",
-    "---",
-    "this is a test",
-    "---",
-    "",
-    "now I am typing some stuff",
-    "end"
-    ]
+main = putStr $ show $ getEntry $ splitOn "\n" txt
+  where txt = "part before this\n---\nthis is a test\n---\n\nnow I am typing some stuff\nend"
